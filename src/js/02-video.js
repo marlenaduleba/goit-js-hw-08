33daa6e4e5457567e375d911b1b253f3f5d33584
Marlena Dulęba <marlena_duleba@o2.pl>
2022-12-11 19:46:18 +0100

    methods from vimeo added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from "@vimeo/player";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+player.on("timeupdate", saveCurrentTime);
+
+function saveCurrentTime(event) {
+    localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
+}
+
+player.setCurrentTime(currentTime);
