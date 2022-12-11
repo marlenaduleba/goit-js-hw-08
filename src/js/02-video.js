63f6a3ef781b1b7d5fb824cad8a83eb576a4f99c
Marlena Dulęba <marlena_duleba@o2.pl>
2022-12-11 22:49:49 +0100

    task-02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,16 @@
-import Player from "@vimeo/player";
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
 
-player.on("timeupdate", saveCurrentTime);
-
-function saveCurrentTime(event) {
+player.on(
+  'timeupdate',
+  throttle(function (event) {
     localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
-}
+  }, 1000)
+);
 
 player.setCurrentTime(currentTime);
