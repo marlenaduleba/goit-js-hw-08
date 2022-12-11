fc39eecedccd118d5a38b7db54b15a8bb4300d67
Marlena Dulęba <marlena_duleba@o2.pl>
2022-12-11 14:56:38 +0100

    taks-01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+makeGallery();
+
+function makeGallery() {
+  const images = galleryItems
+    .map(
+      (item) => `<a class="gallery__item" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`
+    )
+    .join("");
+  gallery.insertAdjacentHTML("afterbegin", images);
+}
+
+let lightbox = new SimpleLightbox(`.gallery a`, {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionDelay: 250,
+});
