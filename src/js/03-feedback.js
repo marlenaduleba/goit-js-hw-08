7ed72045f6e0262c0c83d9f6223269ec7f495765
Marlena Dulęba <marlena_duleba@o2.pl>
2022-12-14 21:08:36 +0100

    task-03 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,33 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const {email, message} = form;
-const LOCALSTORAGE_KEY = "feedback-form-state";
+const { email, message } = form;
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 let storage = {};
 let savedObject = {};
 const storageString = localStorage.getItem(LOCALSTORAGE_KEY);
 const storageObject = JSON.parse(storageString);
 
-    if (storageString !== null) {
-    email.value = storageObject.email;
-    message.value = storageObject.message;  
-    savedObject.email = storageObject.email;
-    savedObject.message = storageObject.message;
+if (storageString !== null) {
+  email.value = storageObject.email;
+  message.value = storageObject.message;
+  savedObject.email = storageObject.email;
+  savedObject.message = storageObject.message;
 }
 
-
-form.addEventListener("input", throttle(saveObject, 500));
+form.addEventListener('input', throttle(saveObject, 500));
 
 function saveObject(event) {
-    event.preventDefault();
-    storage.email = email.value;
-    storage.message = message.value;
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(storage)); 
+  storage.email = email.value;
+  storage.message = message.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(storage));
 }
 
- form.addEventListener("submit", e => {
-    e.preventDefault()
-    savedObject.email = email.value;
-    saveObject.message = message.value;
-    console.log(savedObject);
-    form.reset();
-    localStorage.removeItem(LOCALSTORAGE_KEY);
-})
-
-
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  savedObject.email = email.value;
+  savedObject.message = message.value;
+  console.log(savedObject);
+  form.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+});
